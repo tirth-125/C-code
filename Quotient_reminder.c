1418ff4c19b3e41c2a79833356935d1effa7b3a6
tirth-125 <105578409+tirth-125@users.noreply.github.com>
2022-05-22 12:02:40 +0530

    Create Quotient_reminder.c

---
+++ Quotient_reminder.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    int dividend,divisior;
+    
+    printf("Enter the dividend=");
+    scanf("%d",&dividend);
+    
+    printf("Enter the divisor=");
+    scanf("%d",&divisior);
+    
+    
+    printf("Quotient=%d\n",dividend/divisior);
+    printf("reminder=%d",dividend%divisior);
+    
+    
+    
+    return 0;
+}
