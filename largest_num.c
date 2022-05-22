f5856b3857477d925e2c74343cc23f8762c94c86
tirth-125 <105578409+tirth-125@users.noreply.github.com>
2022-05-22 12:05:49 +0530

    Create largest_num.c

---
+++ largest_num.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2,num3;
+    
+    printf("Enter the num1:");
+    scanf("%d",&num1);
+    
+    printf("Enter the num2:");
+    scanf("%d",&num2);
+    
+    printf("Enter the num3:");
+    scanf("%d",&num3);
+    
+    
+    if(num1>num2 && num1>num3){
+        printf("%d is largest",num1);
+    }
+    else if(num2>num3 && num2>num1)
+    {
+        printf("%d is largest",num2);
+    }
+    else if (num1==num2==num3)
+    {
+        printf("All num are same");
+    }
+    
+    else{
+        printf("%d is largest",num3);
+    }
+    
+    return 0;
+}
