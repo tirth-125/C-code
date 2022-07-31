4930414854c6e45ca1e0a25d4fa9bd487b6e9a4a
tirth-125 <105578409+tirth-125@users.noreply.github.com>
2022-07-31 11:28:04 +0530

    Add files via upload

---
+++ countint.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num,count=0;
+    printf("Enter the num=");
+    scanf("%d",&num);
+
+    while (num!=0)
+    {
+        num=num/10;
+        count++;
+
+        
+    }
+    printf("Count number of digits=%d",count);
+    
+
+    return 0;
+}
---
+++ factminmax.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{   
+
+    int min,max,num,product=1;
+    printf("Enter the number min=");
+    scanf("%d",&min);
+
+
+    printf("Enter the number max=");
+    scanf("%d",&max);
+
+    for (int j = min; j <= max; j++)
+    {
+        product=1; //(doubted)
+        for (int i = 1; i <=j; i++)
+        
+            product=product*i;
+            printf("%d\n",product);
+        
+        
+        
+    }
+    
+    
+
+    return 0;
+}
